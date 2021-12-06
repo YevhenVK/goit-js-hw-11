c1112bbf6428ec826bbe70c94cdf9677dc956a70
YevhenVK <onegin797@gmail.com>
2021-12-06 12:04:22 +0200

    Дз11

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import axios from "axios";
+axios.get('/users')
+  .then(res => {
+    console.log(res.data);
+  });
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
